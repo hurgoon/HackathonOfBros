f419890c8bce03bab7d79a150e139fec7581d7d5
Isaac Kim <myong328@gmail.com>
2018-11-16 10:58:09 +0900

    initial waku has been created

---
+++ ClickNBuy/ClickNBuy/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClickNBuy
+//
+//  Created by Isaac Kim on 16/11/2018.
+//  Copyright © 2018 Isaac Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
