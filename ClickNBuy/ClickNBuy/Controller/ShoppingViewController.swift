394fa4a6fc146b74bf4da4f58729c6b5861307d4
Isaac Kim <myong328@gmail.com>
2018-11-17 15:08:12 +0900

    completed

--- ClickNBuy/ClickNBuy/Controller/ShoppingViewController.swift
+++ ClickNBuy/ClickNBuy/Controller/ShoppingViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 import Alamofire
+import SafariServices
 
 
 
@@ -21,34 +22,40 @@ class ShoppingViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var textLabelForBold: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var productImage: UIImageView!
+    var firstLink = ""
+    
     
     // second item
     @IBOutlet weak var secondImageLabel: UIImageView!
     @IBOutlet weak var secondProductLabel: UILabel!
     @IBOutlet weak var secondPriceLabel: UILabel!
+    var secondLink = ""
     
     // third item
     @IBOutlet weak var thirdImageLabel: UIImageView!
     @IBOutlet weak var thirdProdcutLabel: UILabel!
     @IBOutlet weak var thirdPriceLabel: UILabel!
-    
+    var thirdLink = ""
     
     // fourth item
     @IBOutlet weak var fourthImageLabel: UIImageView!
     @IBOutlet weak var fourthProductLabel: UILabel!
     @IBOutlet weak var fourthPriceLabel: UILabel!
+    var fourthLink = ""
     
     
     // fifth item
     @IBOutlet weak var fifthImageLabel: UIImageView!
     @IBOutlet weak var fifthProductLabel: UILabel!
     @IBOutlet weak var fifthPriceLabel: UILabel!
+    var fifthLink = ""
     
     
     // sixth item
     @IBOutlet weak var sixthImageLabel: UIImageView!
     @IBOutlet weak var sixthProductLabel: UILabel!
     @IBOutlet weak var sixthPriceLabel: UILabel!
+    var sixthLink = ""
     
     
     // seventh item
@@ -138,58 +145,95 @@ class ShoppingViewController: UIViewController, UISearchBarDelegate {
                     print("Image loaded")
                 }
             }
-            
+          
             
             DispatchQueue.main.async {
                 
                 // first item label
                 self.textLabelForBold.text = shoppingList.items[0].title
                 self.priceLabel.text = "Price: ₩ \(shoppingList.items[0].lprice)"
+                self.firstLink = shoppingList.items[0].link
                 
                 // second item label
                 self.secondProductLabel.text = shoppingList.items[1].title
                 self.secondPriceLabel.text = "Price: ₩ \(shoppingList.items[1].lprice)"
+                self.secondLink = shoppingList.items[1].link
+                
                 
                 // third item label
                 self.thirdProdcutLabel.text = shoppingList.items[2].title
                 self.thirdPriceLabel.text = "Price: ₩ \(shoppingList.items[2].lprice)"
+                self.thirdLink = shoppingList.items[2].link
                 
                 // fourth item label
                 self.fourthProductLabel.text = shoppingList.items[3].title
                 self.fourthPriceLabel.text = "Price: ₩ \(shoppingList.items[3].lprice)"
+                self.fourthLink = shoppingList.items[3].link
                 
                 // fifth item label
                 self.fifthProductLabel.text = shoppingList.items[4].title
                 self.fifthPriceLabel.text = "Price: ₩ \(shoppingList.items[4].lprice)"
+                self.fifthLink = shoppingList.items[4].link
                 
                 // sixth item label
                 self.sixthProductLabel.text = shoppingList.items[5].title
                 self.sixthPriceLabel.text = "Price: ₩ \(shoppingList.items[5].lprice)"
+                self.sixthLink = shoppingList.items[6].link
                 
-                
+//                self.thirdLink = shoppingList.items[0].link
                 
             }
-//
-//            let url = URL(string: "https://loremflickr.com/860/640/cat")!
-//            if let data = try? Data(contentsOf: url) {
-//                print("image downloaded")
-//                imageView.image = UIImage(data: data)
             
             
             
-//            for i in 0...(shoppingList.items.count - 1) {
-//                print("title: ", shoppingList.items[i].title)
-//                print("Lowest Price: " , shoppingList.items[i].lprice)
-//                print("Buy Link: ", shoppingList.items[i].link)
-//                print("image Link: ", shoppingList.items[i].image)
-//
-//
-//            }
+            
+
     })
         task.resume()
       
     }
     
 
+    
+    @IBAction func firstButtonDidTap(_ sender: UIButton) {
 
+        guard let url = URL(string: firstLink) else { return }
+        let safari = SFSafariViewController(url: url)
+        present(safari, animated: true)
+    }
+    
+    @IBAction func secondButtonDidTap(_ sender: UIButton) {
+        
+        guard let url = URL(string: secondLink) else { return }
+        let safari = SFSafariViewController(url: url)
+        present(safari, animated: true)
+        
+    }
+    
+    @IBAction func thirdButtonDidTap(_ sender: UIButton) {
+        guard let url = URL(string: thirdLink) else { return }
+        let safari = SFSafariViewController(url: url)
+        present(safari, animated: true)
+    }
+    
+    @IBAction func fourthButtonDidTap(_ sender: UIButton) {
+        guard let url = URL(string: fourthLink) else { return }
+        let safari = SFSafariViewController(url: url)
+        present(safari, animated: true)
+    }
+    
+    @IBAction func fifthButtonDidTap(_ sender: UIButton) {
+        guard let url = URL(string: fifthLink) else { return }
+        let safari = SFSafariViewController(url: url)
+        present(safari, animated: true)
+    }
+    
+    @IBAction func sixthButtonDidTap(_ sender: UIButton) {
+        guard let url = URL(string: sixthLink) else { return }
+        let safari = SFSafariViewController(url: url)
+        present(safari, animated: true)
+    }
+    
+    
+    
 }
